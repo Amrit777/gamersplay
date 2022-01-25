0fe2e2cfb661c277d908ad2ae313b0831c7a8dec
amrit singh <amritsinghh777@gmail.com>
2022-01-26 02:40:48 +0530

    changes

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        '/auth/login',
-        '/auth/register'
+        // '/auth/login',
+        // '/auth/register'
     ];
 }
