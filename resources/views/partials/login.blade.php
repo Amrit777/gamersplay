2f14f73aea5958893bf1947cf9eef177b4726c38
amrit singh <amritsinghh777@gmail.com>
2022-01-26 16:59:02 +0530

    changes

--- resources/views/partials/login.blade.php
+++ resources/views/partials/login.blade.php
@@ -225,7 +225,7 @@
     $(document).ready(function() {
         $.ajaxSetup({
             headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content');
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
             }
         });
         console.log("indidede");
