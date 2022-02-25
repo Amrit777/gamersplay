92f5980bce2acb5971ad32876a34cbf2dbd3b0ea
amrit singh <amritsinghh777@gmail.com>
2022-02-26 03:24:04 +0530

    changes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -34,7 +34,7 @@ class PostController extends Controller
     public function createPost(Request $request)
     {
         $request->validate([
-            'content' => 'required|max:1024|string',
+            'content' => 'max:1024|string',
             'postPhotos.*' => 'mimes:jpeg,jpg,x-png,png',
             'postPhotos' => 'max:4',
             'postVideo' => 'mimes:mp4',
